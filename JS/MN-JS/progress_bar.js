27cfc4bb94d7486448dca8d393e05494d191cb87
WebTogz <antonin.carette@gmail.com>
2014-08-25 19:53:04 +0200

    POO

--- JS/MN-JS/progress_bar.js
+++ JS/MN-JS/progress_bar.js
@@ -1,10 +1,23 @@
-function init_progress_bar () {
+/**
+ * Object to represents a progress bar.<br/>
+ * The progress_bar object represents the HTML 'progress' object.<br/>
+ * This object serves to display the progress of the principal request.<br/>
+ * Exemple: <progress>
+ * @constructor
+ */
+function progress_bar() {}
+
+/**
+ * Function to init the object.<br/>
+ * The progress_bar object will be display as first child of the 'display_results' div.
+ */
+progress_bar.prototype.init_progress_bar = function() {
 
 	var display_results_node = document.getElementById('display_results');
 
 	var div = document.createElement("div");
 	div.id = "div_progress_bar";
-	div.appendChild(document.createTextNode("Please wait..."));
+	div.appendChild(document.createTextNode("Computing - please wait..."));
 	div.appendChild(document.createElement('br'));
 
 	var progress_bar = document.createElement("progress");
@@ -15,24 +28,36 @@ function init_progress_bar () {
 
 	display_results_node.appendChild(div);
 
-}
-
-function evolve_progress_bar (value) {
-
-	document.getElementById("progress_bar").value = value;
+	this.progress_bar = document.getElementById("progress_bar");
 
 }
 
-function upgrade_progress_bar () {
-
-	var value = document.getElementById("progress_bar").value;
-	document.getElementById("progress_bar").value = (value+0.01);
-
+/**
+ * Function to evolve the value of the object.
+ * @param int value New value of the progress_bar object
+ */
+progress_bar.prototype.evolve_progress_bar = function(value) {
+	this.progress_bar.value = value;
 }
- 
-function clean_progress_bar () {
 
-	//Removal progress bar
-	document.getElementById("div_progress_bar").remove();
+/**
+ * Function to get the value of the object.
+ */
+progress_bar.prototype.get_value_progress_bar = function() {
+	return this.progress_bar.value;
+}
 
+/**
+ * Function to upgrade the value of the object.<br/>
+ * The gap of the upgrade is 0.01.
+ */
+progress_bar.prototype.upgrade_progress_bar = function() {
+	this.evolve_progress_bar(this.get_value_progress_bar() + 0.01);
+}
+
+/**
+ * Function to clean/remove the object.
+ */
+progress_bar.prototype.clean_progress_bar = function() {
+	this.progress_bar.remove();
 }
