cfe7505ed82ece7aa7af277017ad1e19ae283b06
WebTogz <antonin.carette@gmail.com>
2014-08-25 23:02:05 +0200

    Addition of a new class: 'similar_movies' -> object which contains all the similar movies of the first one

---
+++ JS/MN-JS/similar_movies.js
@@ -0,0 +1,23 @@
+/**
+ * Array object which contains all the similar movies computed<br/>
+ * Each similar movie MUST BE a movie_object object!
+ * @constructor
+ */
+function similar_movies() {
+	this.reset();
+}
+
+/**
+ * Function to reset the array of similar movies
+ */
+similar_movies.prototype.reset = function() {
+	this.array = new Array();
+}
+
+/**
+ * Function to add a similar movie in the array attribute
+ * @param movie_object similar_movie A movie_object object, which represents a similar movie for the one entered
+ */
+similar_movies.prototype.add_similar_movie = function(similar_movie) {
+	this.array.push(similar_movie);
+}
